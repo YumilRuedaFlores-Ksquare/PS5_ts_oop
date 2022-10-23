73c192eaeb86e5577442379ced217acb4d444db6
YumilRuedaFlores-Ksquare <Yumil.Flores@itksquare.edu.mx>
2022-10-23 20:39:05 +0200

    DONE correctly

--- prob_2/index.ts
+++ prob_2/index.ts
@@ -24,7 +24,11 @@ class Matrix {
     }
 
     public  setElement(i:number,j:number,value:number){
+        if(i < this._row && j<this._column){
         this._elements[i][j] = value;
+        }else{
+            console.log('Out of the matrix range');
+        }
     }
 
     public add(M:Matrix){
@@ -40,22 +44,25 @@ class Matrix {
     }
     public multiply(M:Matrix){
         let sum = 0;
-        const Mresult = new Matrix(this.column, M._row);
-        if(this._row == M._column ){
-            console.log('Ok');
-            for(let k:number = 0; k<M._row;k++ ){
+        const Mresult = new Matrix(this._row, M._column);
+        if(this._column == M._row ){
+            console.log('SIP'); //I add this just for show something in my example
+            for(let k:number = 0; k<M._column;k++ ){
                 for(let i: number = 0; i < this._row; i++) {
+                    let value = 0;
                     for(let j: number = 0; j< this._column; j++) {
-                        let value = this._elements[i][j] * M._elements[k][j] + Mresult._elements[j][k];
-                        Mresult.setElement(j,k,value);
+                        value += this._elements[i][j] * M._elements[j][k];
                     }
+                    Mresult.setElement(i,k,value);
                 }
             }
             this._row = Mresult.row;
             this._column = Mresult.column;
+            this._elements =[];
             this._elements = Mresult._elements;
+
         }else{
-            console.log('NOOP'); 
+            console.log('NOOP');
         }
     }
 
@@ -64,17 +71,50 @@ class Matrix {
 const m1 = new Matrix(2,2);
 m1.setElement(1,1,10);
 const m2 = new Matrix(2,2);
+const m5 = new Matrix(2,1);
+
 m2.setElement(1,0,10);
 m1.add(m2);
-const m3 = new Matrix(2,2);
-const m4 = new Matrix(1,2);
-m3.setElement(0,0,1);
+
+m1.setElement(0,0,1);
+m1.setElement(0,1,2);
+
+m1.setElement(1,0,3);
+m1.setElement(1,1,4);
+
+m5.setElement(0,0,1);
+m5.setElement(1,0,2);
+m1.multiply(m5);
+console.log('EX1: ',m1);
+
+const m3 = new Matrix(3,3);
+const m4 = new Matrix(3,2);
+m3.setElement(0,0,3);
 m3.setElement(0,1,2);
-m3.setElement(1,0,3);
-m3.setElement(1,1,4);
-m4.setElement(0,0,1);
-m4.setElement(0,1,2);
+m3.setElement(0,2,1);
+
+m3.setElement(1,0,1);
+m3.setElement(1,1,1);
+m3.setElement(1,2,3);
+
+m3.setElement(2,0,0);
+m3.setElement(2,1,2);
+m3.setElement(2,2,1);
+
+
+m4.setElement(0,0,2);
+m4.setElement(0,1,1);
+
+m4.setElement(1,0,1);
+m4.setElement(1,1,0);
+
+m4.setElement(2,0,3);
+m4.setElement(2,1,2);
+
 m3.multiply(m4);
-console.log(m3)
+console.log('eX2: ', m3)
 
 
+//Example 3
+m3.multiply(m1);
+m1.multiply(m3);
